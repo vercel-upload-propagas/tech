a3a677d27115b6642ead5b8ac791f723f3471824
Lucas Fernando Quinato De Camargo <lfqcamargo@saomanoel.com.br>
2026-03-12 08:43:09 -0300

    ADS

--- src/app/sitemap_index.xml/route.ts
+++ src/app/sitemap_index.xml/route.ts
@@ -2,11 +2,13 @@ import { clientEnv } from "@/env";
 
 export async function GET() {
   const baseUrl = clientEnv.NEXT_PUBLIC_SITE_URL;
+  const lastmod = new Date().toISOString();
 
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
 <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <sitemap>
     <loc>${baseUrl}/sitemap.xml</loc>
+    <lastmod>${lastmod}</lastmod>
   </sitemap>
 </sitemapindex>`;
 
